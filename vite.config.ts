a39817b424b758bd22a3781b135b2fe00a5309cc
Arten360 <martin.stolz@revalenz.de>
2025-06-15 22:13:11 +0200

    API Proxy überarbeitet

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,13 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    proxy: {
+      '/api/voting': {
+        target: 'https://script.google.com/macros/s/AKfycbw6T6Nmx0L_72v5n9a-Z5etbXlA4s2-yVFLBwKllGshcg26W2R1-e2Lq8BtTqU2kE3wJQ',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/voting/, '/exec'),
+      },
+    },
   },
   plugins: [
     react(),
